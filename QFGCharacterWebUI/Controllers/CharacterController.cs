ae9d3378d5cfdbb8bfbb46c33b1d45d18803a2d7
Adam Plocher <aplocher@gmail.com>
2013-02-07 17:29:29 -0800

    Made several updates to the web UI

--- QFGCharacterWebUI/Controllers/CharacterController.cs
+++ QFGCharacterWebUI/Controllers/CharacterController.cs
@@ -17,6 +17,8 @@ namespace QFGCharacterWebUI.Controllers
         [HttpPost]
         public ActionResult General(QfgCharacter qfgCharacter)
         {
+            //qfgCharacter.SetMaxValues();
+            //qfgCharacter.CharacterName = "Blah2";
             return View(qfgCharacter);
         }
 
@@ -28,7 +30,7 @@ namespace QFGCharacterWebUI.Controllers
 
         [HttpPost]
         public ActionResult Inventory(QfgCharacter qfgCharacter)
-        { 
+        {
             return View(qfgCharacter);
         }
 
@@ -36,6 +38,47 @@ namespace QFGCharacterWebUI.Controllers
         public ActionResult MagicStats(QfgCharacter qfgCharacter)
         {
             return View(qfgCharacter);
+        }
+
+        [HttpPost]
+        public ActionResult MaxStats(QfgCharacter qfgCharacter)
+        {
+            ModelState.Remove("Agility");
+            ModelState.Remove("WeaponUse");
+            ModelState.Remove("Intelligence");
+            ModelState.Remove("Strength");
+            ModelState.Remove("Vitality");
+            ModelState.Remove("Luck");
+            ModelState.Remove("Communication");
+            ModelState.Remove("Parry");
+            ModelState.Remove("Dodge");
+            ModelState.Remove("Stealth");
+            ModelState.Remove("PickLocks");
+            ModelState.Remove("Throwing");
+            ModelState.Remove("Climbing");
+            ModelState.Remove("Magic");
+            ModelState.Remove("Acrobatics");
+
+            ModelState.Remove("MagicSkillCalm");
+            ModelState.Remove("MagicSkillDazzle");
+            ModelState.Remove("MagicSkillDetect");
+            ModelState.Remove("MagicSkillFetch");
+            ModelState.Remove("MagicSkillFlame");
+            ModelState.Remove("MagicSkillForceBolt");
+            ModelState.Remove("MagicSkillLevitate");
+            ModelState.Remove("MagicSkillOpen");
+            ModelState.Remove("MagicSkillReversal");
+            ModelState.Remove("MagicSkillTrigger");
+            ModelState.Remove("MagicSkillZap");
+
+            ModelState.Remove("InventoryDaggers");
+            ModelState.Remove("InventoryHealingPotions");
+            ModelState.Remove("InventoryMagicPotions");
+            ModelState.Remove("InventoryVigorPotions");
+
+            qfgCharacter.SetMaxValues();
+
+            return View("General", qfgCharacter);
         }
     }
 }
---
+++ QFGCharacterWebUI/HtmlHelpers/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QFGCharacterWebUI.HtmlHelpers
+{
+    public static class ExtensionMethods
+    {
+        public static SelectList ToSelectList<TEnum>(this TEnum enumObj)
+        {
+            var values = from TEnum e in Enum.GetValues(typeof(TEnum))
+                         select new { Id = e, Name = e.ToString() };
+
+            return new SelectList(values, "Id", "Name", enumObj);
+        }
+    }
+}
