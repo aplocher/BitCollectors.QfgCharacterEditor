ba422959fc5ac9502fdb09e2cad9f14a1a2ade1d
Adam Plocher <aplocher@gmail.com>
2013-02-20 20:35:39 -0800

    Renamed project folders to match new namespace names

---
+++ BitCollectors.QfgCharacterEditor.Tests/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitCollectors.QfgCharacterEditor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestQfg1FighterMaxValues()
+        {
+            Assert.AreEqual<string>("Hello", "Hello");
+        }
+
+        [TestMethod]
+        public void TestQfg1MagicMaxValues()
+        {
+            Assert.AreEqual<string>("Test", "Test");
+        }
+
+        [TestMethod]
+        public void TestQfg1ThiefMaxValues()
+        {
+            Assert.AreEqual<string>("Test", "Test");
+        }
+
+        [TestMethod]
+        public void TestQfg2FighterMaxValues()
+        {
+            Assert.AreEqual<string>("Hello", "Hello");
+        }
+
+        [TestMethod]
+        public void TestQfg2MagicMaxValues()
+        {
+            Assert.AreEqual<string>("Test", "Test");
+        }
+
+        [TestMethod]
+        public void TestQfg2ThiefMaxValues()
+        {
+            Assert.AreEqual<string>("Test", "Test");
+        }
+
+        [TestMethod]
+        public void TestQfg2PaladinMaxValues()
+        {
+            Assert.AreEqual<string>("Test", "Test");
+        }
+
+
+        [TestMethod]
+        public void TestMaxValueReset()
+        {
+            // TODO: Set Game to QFG2
+            // SetMaxValues
+            // Set Game to QFG1
+            // Check that max values are not greater than 100
+
+            Assert.AreEqual<bool>(true, true);
+        }
+    }
+}
